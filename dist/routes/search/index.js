7f455a9c6c6d1069b6f5a8c232d8b94ca552c2bd
Kaio Vinycius Braga <kaiovbraga2001@gmail.com>
2021-08-22 18:57:06 -0300

    feat: Substituindo 'dist' por 'build'

--- dist/routes/search/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _express = require('express');
-var _SearchController = require('../../controllers/SearchController'); var _SearchController2 = _interopRequireDefault(_SearchController);
-
-const route = _express.Router.call(void 0, );
-
-route.get("/search", _SearchController2.default.index);
-
-exports. default = route;
